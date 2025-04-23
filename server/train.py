0b4fa63befe88b1d5f2da9b4ba353d07d4349404
ADJARO <119491882+Adjaro@users.noreply.github.com>
2025-04-23 15:42:50 +0200

    update

--- server/train.py
+++ server/train.py
@@ -3,6 +3,8 @@ import pandas as pd
 import  pickle
 from sklearn.pipeline import make_pipeline
 from   sklearn.preprocessing import StandardScaler
+import os
+
 
 scaler = StandardScaler()
 model = make_pipeline(scaler, DecisionTreeClassifier(random_state=42))
@@ -10,12 +12,15 @@ model = make_pipeline(scaler, DecisionTreeClassifier(random_state=42))
 # Charger le jeu de données Iris
 data = pd.read_csv("data/iris.csv")
 
-X = data.drop("species", axis=1)
-y = data["species"]
+X = data.drop("label", axis=1)
+y = data["label"]
 
 # Entraîner le modèle
 model.fit(X, y)
 
+#creer le  dossier model si il n'existe pas
+if not os.path.exists("model"):
+    os.makedirs("model")
 # Sauvegarder le modèle entraîné dans un fichier .pkl
 with open("model/model.pkl", "wb") as f:
     pickle.dump(model, f)
